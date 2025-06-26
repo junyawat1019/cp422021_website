33d5709e450d0fb1f96ce13bd076698b3251e677
junyawat1019 <junyawat1019@gmail.com>
2025-06-26 23:05:05 +0700

    new update

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -13,7 +13,7 @@ module.exports = {
   rules: {
     "no-restricted-globals": ["error", "name", "length"],
     "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    "quotes": ["error", "double", { "allowTemplateLiterals": true }],
   },
   overrides: [
     {
@@ -25,4 +25,4 @@ module.exports = {
     },
   ],
   globals: {},
-};
+}
